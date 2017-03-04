be6ccb36110bdc082316721315612859c9f5f5f0
zl <810881283@qq.com>
2017-03-04 20:39:43 +0800

    fix overflow

--- front/src/app/pipes/num2str.pipe.ts
+++ front/src/app/pipes/num2str.pipe.ts
@@ -1,14 +1,19 @@
-import {Pipe} from '@angular/core';
+import { Pipe } from '@angular/core';
 
-@Pipe({name: "num2str"})
+@Pipe({ name: "num2str" })
 export class Num2strPipe {
-  public transform(input:number, arr:Array<string|Object>, startWith:number = 0, name:string = 'name') {
+  public transform(input: number, arr: Array<string | Object>, startWith: number = 0, name: string = 'name') {
     input -= startWith;
     if (typeof arr[0] === 'string') {
       return arr[input];
     } else {
-      return arr[input][name];
+      console.log(input);
+      console.log(arr);
+      try {
+        return arr.filter((i) => i.id == input)[0][name];
+      } catch (e) {
+        return '';
+      }
     }
   }
 }
-
